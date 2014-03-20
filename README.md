8952e390b55defb096f5faefaaab8c8a3f6df9ff
Johannes Choo <jhanschoo@gmail.com>
2014-03-20 14:48:07 -0700

    add examples to README, add TODOs to arty.js

--- README.md
+++ README.md
@@ -2,3 +2,29 @@ arty
 =====
 
 tiny javascript library for asserting types on function arguments
+
+To use:
+
+    arty = require('arty');
+
+    var add = function (n1, n2) {
+      return n1 + n2;
+    };
+
+    console.log(add(3, 4));
+    => 7
+    console.log(add(3, {}))
+    => 3[object Object]
+
+    var typed_add = arty('number', 'number')(function (n1, n2) {
+      return n1 + n2;
+    });
+
+    var bin_num_arty = arty('number', 'number');
+    var typed_subtract = bin_num_arty(function (n1, n2) {
+      return n1 - n2;
+    });
+
+    var multiplication = function (n1, n2) { return n1 * n2; };
+    var typed_multiplication = arty('number', 'number')(multiplication);
+
--- arty.js
+++ arty.js
@@ -1,20 +1,31 @@
-var arty = function (/* param_types */) {
-  var type_constraints = arguments;
-  var check_types = function(a) {
-    // check arguments pattern matching arguments with type_constraints
-    for (var i = 0, ii = type_constraints.length; i < ii; i++) {
-      var t = type_constraints[i];
-      var p = a[i];
-      // TODO(jhanschoo): implement the actual type checking for each argument.
-    }
-  }
-  return function (wrapped_function) {
-    return function (/* params */) {
-      check_types(arguments);
-      return wrapped_function.apply(this, arguments);
-    }
-  }
+var t = function (flag) {
+  return function (/* param_types */) {
+    var type_constraints = arguments;
+    var check_types = function(a) {
+      // check arguments pattern matching arguments with type_constraints
+      for (var i = 0, ii = type_constraints.length; i < ii; i++) {
+        var t = type_constraints[i];
+        var p = a[i];
+        // TODO(jhanschoo): implement the actual type checking for each argument.
+      }
+    };
+    return function (wrapped_function) {
+      return function (/* params */) {
+        check_types(arguments);
+        return wrapped_function.apply(this, arguments);
+      };
+    };
+  };
 };
+
+var arty = t();
+
+arty.rty = arty
+arty.arty = arty
+arty.farty = t('f');
+arty.sarty = t('s');
+
+// TODO(jhanschoo): arty.message.error = '....'
 
 module.exports = arty;
 
